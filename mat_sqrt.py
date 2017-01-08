860f30db573fccad90deef27ea062ed1978e188f
Jonas Adler <jonasadl@kth.se>
2017-01-08 22:02:51 +0100

    Remove mat_sqrt.py file

--- mat_sqrt.py
+++
@@ -1,7 +0,0 @@
-from __future__ import division
-from sympy import *
-
-x, y, z, a, b, c = symbols('x y z a b c', real=True)
-
-solution = solve_poly_system([x*x + y*y - a, y*y + z*z - c, x*y + y*z - b],
-                             x, y, z)
